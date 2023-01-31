81c6759b555cc71cd98589735d74088720ae622a
Corentin Radenac <corentin.radenac@gmail.com>
2023-02-01 00:51:04 +0100

    add css home page

--- components/HomePage/Home.jsx
+++ components/HomePage/Home.jsx
@@ -1,50 +1,89 @@
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image, Text, Dimensions } from "react-native";
 import { Button } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Home = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={() => navigation.navigate("CatchPage")}
+      <LinearGradient
+        style={styles.homePage}
+        locations={[0, 1]}
+        colors={["#aa0707", "#280a33"]}
       >
-        Make me a catch please
-      </Button>
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={() => navigation.navigate("DescriptionPage")}
-      >
-        Make me a description
-      </Button>
-      <StatusBar style="auto" />
+        <Image
+          style={styles.logoIcon}
+          resizeMode="cover"
+          source={require("../../assets/logo.png")}
+        />
+        <Button
+          style={[styles.catchButton, styles.buttonStyle]}
+          onPress={() => navigation.navigate("CatchPage")}
+        >
+          <Text style={styles.typoTextButton}>{`Make me catch phrase`}</Text>
+          <Image
+            source={require("../../assets/description-chevron-right.png")}
+          />
+        </Button>
+
+        <Button
+          style={[styles.descriptionButton, styles.buttonStyle]}
+          onPress={() => navigation.navigate("DescriptionPage")}
+        >
+          <Text style={styles.typoTextButton}>{`Make me a description`}</Text>
+          <Image
+            source={require("../../assets/description-chevron-right.png")}
+          />
+        </Button>
+      </LinearGradient>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  homePage: {
+    width: "100%",
+    height: "100%",
+  },
+
+  logoIcon: {
+    display: "flex",
+    height: "55%",
+    width: "100%",
+  },
+
+  catchButton: {
+    top: "60%",
+    bottom: "35%",
+  },
+
+  descriptionButton: {
+    top: "70%",
+    bottom: "25%",
+  },
+
+  buttonStyle: {
+    left: "3%",
+    right: "3%",
+    width: "95%",
+    height: "6%",
+    position: "absolute",
+    borderWidth: 3,
+    borderColor: "black",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  button: {
+
+  typoTextButton: {
     alignItems: "center",
-    justifyContent: "center",
-    position: "relative",
-    width: "90%",
-    height: "10%",
-    textAlign: "center",
-    margin: "3%",
-  },
-  arrowContainer: {
-    alignSelf: "flex-end",
-    justifyContent: "flex-end",
+    display: "flex",
+    color: "white",
   },
 });
 
